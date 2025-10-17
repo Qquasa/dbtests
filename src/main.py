617b5a9ecd39f6d40f686bdd59a79fda2d165311
Qquasa <exnikita3@gamil.com>
2025-10-17 21:52:19 +0200

    les8 complete

--- src/main.py
+++ src/main.py
@@ -1,15 +1,33 @@
+import asyncio
 import os
 import sys
+import uvicorn
+
 sys.path.insert(1, os.path.join(sys.path[0], '..'))
 
 from queries.orm import SyncORM
 from queries.core import SyncCORE
 
-SyncORM.create_tables()
-SyncORM.insert_data()
+async def main():
 
-SyncORM.select_worker()
-SyncORM.update_worker()
+    if "--core" in sys.argv and "--sync" in sys.argv:
+        SyncCORE.create_tables()
+        SyncCORE.insert_worker()
+        SyncCORE.select_worker()
 
-# SyncCORE.select_worker()
-# SyncCORE.update_worker()
+    # ORM
+    elif "--orm" in sys.argv and "--sync" in sys.argv:
+        SyncORM.create_tables()
+        SyncORM.insert_data()
+        SyncORM.select_worker()
+        SyncORM.update_worker()
+        SyncORM.insert_resume()
+        SyncORM.select_resume_avg_compensation()
+
+if __name__ == "__main__":
+    asyncio.run(main())
+    if "--webserver" in sys.argv:
+        uvicorn.run(
+            app="src.main:app",
+            reload=True,
+        )
--- src/queries/orm.py
+++ src/queries/orm.py
@@ -1,7 +1,7 @@
-from sqlalchemy import text, insert, select, update, delete
-
+from sqlalchemy import text, insert, select, update, delete, func, cast, Integer, and_
+from sqlalchemy.orm import aliased
 from database import sync_engine, async_engine, session_factory, Base
-from models import metadata_obj, WorkerORM
+from models import metadata_obj, WorkerORM, ResumeORM, Workload
 
 class SyncORM:
     @staticmethod
@@ -40,3 +40,115 @@ class SyncORM:
             worker_wolk.username = new_username
             session.refresh(worker_wolk)
             session.commit()
+    
+    @staticmethod
+    def insert_resume():
+        with session_factory() as session:
+            resume_bobr1 = ResumeORM(
+                 title="Python Junior Developer", compensation=50000, workload=Workload.fulltime, worker_id=1)
+            resume_bobr2 = ResumeORM(
+                title="Python Разработчик", compensation=150000, workload=Workload.fulltime, worker_id=1)
+            resume_wolk1 = ResumeORM(
+                title="Python Data Engineer", compensation=250000, workload=Workload.parttime, worker_id=2)
+            resume_wolk2 = ResumeORM(
+                title="Data Scientist", compensation=300000, workload=Workload.fulltime, worker_id=2)
+            session.add_all([resume_bobr1, resume_bobr2,
+                             resume_wolk1, resume_wolk2])
+            session.commit()
+
+    @staticmethod
+    def select_resume_avg_compensation(like_language: str = "Python"):
+        """
+        select workload, avg(compensation)::int as avg_compensation
+        from resumes
+        where title like '%Python%' and compensation > 40000
+        group by workload
+        having avg(compensation) > 70000
+        """
+        with session_factory() as session:
+            query = (
+                select(
+                    ResumeORM.workload,
+                    cast(func.avg(ResumeORM.compensation), Integer).label("avg_compensation"),
+                )
+                .filter(and_(
+                    ResumeORM.title.contains(like_language),
+                    ResumeORM.compensation > 40000,
+                ))
+                .group_by(ResumeORM.workload)
+            )
+        print(query.compile(compile_kwargs={'literal_binds': True}))
+        res = session.execute(query)
+        result = res.all()
+        print(result)
+
+    @staticmethod
+    def insert_additional_resume():
+        with session_factory() as session:
+            worker = [
+                {"username": "Fish"},
+                {"username": "Cat"},
+                {"username": "Dog"},
+            ]
+            resume = [
+                {"title": "Python программист", "compensation": 60000, "workload": "fulltime", "worker_id": 3},
+                {"title": "Machine Learning Engineer", "compensation": 70000, "workload": "parttime", "worker_id": 3},
+                {"title": "Python Data Scientist", "compensation": 80000, "workload": "parttime", "worker_id": 4},
+                {"title": "Python Analyst", "compensation": 90000, "workload": "fulltime", "worker_id": 4},
+                {"title": "Python Junior Developer", "compensation": 100000, "workload": "fulltime", "worker_id": 5},
+            ]
+            insert_worker = insert(WorkerORM).values(worker)
+            insert_resume = insert(ResumeORM).values(resume)
+            session.execute(insert_worker)
+            session.execute(insert_resume)
+            session.commit()
+
+    @staticmethod
+    def join_cte_subquery_window_func():
+        """
+        WITH helper2 AS (
+            SELECT *, compensation-avg_workload_compensation AS compensation_diff
+            FROM 
+            (SELECT
+                w.id,
+                w.username,
+                r.compensation,
+                r.workload,
+                avg(r.compensation) OVER (PARTITION BY workload)::int AS avg_workload_compensation
+            FROM resumes r
+            JOIN workers w ON r.worker_id = w.id) helper1
+        )
+        SELECT * FROM helper2
+        ORDER BY compensation_diff DESC;
+        """
+        with session_factory() as session:
+            w = aliased(WorkerORM)
+            r = aliased(ResumeORM)
+            subq = (
+                select(
+                    r,
+                    w,
+                    func.avg(r.compensation).over(partition_by=r.workload).cast(Integer).label("avg_workload_compensation"),
+                )
+                # .select_from(r)
+                .join(r, r.worker_id == w.id).subquery("helper1")
+            )           
+            cte = (
+                select(
+                    subq.c.worker_id,
+                    subq.c.username,
+                    subq.c.compensation,
+                    subq.c.workload,
+                    subq.c.avg_workload_compensation,
+                    (subq.c.compensation - subq.c.avg_workload_compensation).label("compensation_diff"),
+                )
+                .cte("helper2")
+            )
+            query = (
+                select(cte)
+                .order_by(cte.c.compensation_diff.desc())
+            )
+
+            res = session.execute(query)
+            result = res.all()
+            print(f"{len(result)=}. {result=}")
